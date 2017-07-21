bac5d037284e3005cad6ba2836cee4af09d9440f
Tejas Shah <tejasanilshah@gmail.com>
2017-07-21 17:56:31 +0530

    Working grid and pan gesture recogniser

--- animatedGrid/ViewController.swift
+++ animatedGrid/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let screenWidth = view.frame.width
+        let screenHeight = view.frame.height
+        let numberOfColumns = 10
+        let cellViewWidth = screenWidth/CGFloat(numberOfColumns)
+        let numberOfRows = Int(screenHeight/cellViewWidth)
+        for j in 1...numberOfRows{
+            for i in 1...numberOfColumns{
+                let cellView = UIView(frame: CGRect(x: CGFloat(i-1)*(cellViewWidth), y: 25 + CGFloat(j-1)*(cellViewWidth) , width: cellViewWidth, height: cellViewWidth))
+                cellView.backgroundColor = randomColor()
+                cellView.layer.borderWidth = 1
+                cellView.layer.borderColor = UIColor.white.cgColor
+                view.addSubview(cellView)
+            }
+        }
+        
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func handlePan(gesture: UIPanGestureRecognizer) -> Void {
+        let location = gesture.location(in: view)
+        print(location)
     }
-
-
+    
+    fileprivate func randomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
 }
 
